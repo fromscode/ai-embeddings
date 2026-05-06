c8af089de6825afaafbd95943feff75f83f15d27
fromscode <chakrabortyshubham00@gmail.com>
2026-05-06 12:29:06 +0530

    Connection established

--- index.ts
+++ index.ts
@@ -1,1 +1,18 @@
-console.log("hello world");
+import { GoogleGenAI } from "@google/genai";
+
+const apiKey = process.env.GEMINI_API_KEY!;
+
+const ai = new GoogleGenAI({apiKey, apiVersion: "v1alpha"});
+
+async function main() {
+    const response = await ai.models.generateContentStream({
+        model: "gemini-3.1-flash-lite-preview",
+        contents: "Why is the sky blue?"
+    })
+
+    for await (const chunk of response) {
+        console.log(chunk.text + "\n");
+    }
+}
+
+main();
